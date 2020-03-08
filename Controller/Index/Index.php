b4b09d1f05d2616d39a09dffaa46775adba53f18
Rukhsar <rukhsar.man@gmail.com>
2020-03-08 21:55:50 +0000

    frontend route added

---
+++ Controller/Index/Index.php
@@ -0,0 +1,33 @@
+<?php
+namespace Rukhsar\Faq\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+    /**
+     * View page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->_pageFactory->create(
+            \Magento\Framework\Controller\ResultFactory::TYPE_PAGE
+        );
+    }
+}
